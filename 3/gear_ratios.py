43431b4d3c39e804ec07a342ea97dc02a5129b04
Perceval ARENOU <perceval.arenou@bilberry.io>
2023-12-13 22:55:37 +0000

    day3: use dict to store data

--- 3/gear_ratios.py
+++ 3/gear_ratios.py
@@ -1,4 +1,4 @@
-import numpy as np
+#!/usr/bin/python3
 
 # for each nuber found extract all surrounding chars and join as a string
 # then find symbols in string to detect of it is a part number
@@ -7,46 +7,36 @@ import numpy as np
 # we add an extra border of dots around the input
 
 """
-For those using Python, an even easier option is to store the grid in a dict with either (x,y) tuples or complex (x+y*1j) coordinates. When querying from the dict, use blah.get(coord, '.') and you'll never have an out of bounds concern. Then just iterate over min - 1 to max + 1 for both dimensions.
-7
-User avatar
-level 2
-rabuf
-·
-9 days ago
+For those using Python, an even easier option is to store the grid in a dict with either (x,y) 
+tuples or complex (x+y*1j) coordinates. When querying from the dict, use blah.get(coord, '.') 
+and you'll never have an out of bounds concern. Then just iterate over min - 1 to max + 1 for both dimensions.
 
-You can also use collections.defaultdict and a custom function that returns whatever the desired default value is. In this case you could do something like:
+You can also use collections.defaultdict and a custom function that returns whatever
+the desired default value is. In this case you could do something like:
 
 from collections import defaultdict
 grid = collections.defaultdict(lambda: '.')
 print(grid[20]) # => .
 
 Or whatever you want the default to be.
-3
-User avatar
-level 3
-Anceps2
-·
-9 days ago
-
-You can write:
-
 grid = collections.defaultdict(str)
 
 as str is a function (well, a constructor) that returns an empty string.
 """
 
 
-def is_part_number(position: int) -> bool:
-    return False
+# we will store the grid in a dict in order to take advantage of defaultdict
+def default_factory(coordinates: tuple) -> str:
+    return "."
 
 
-def parse_data(data: str) -> np.matrix:
+# LEGACY
+def parse_data_as_table(data: str) -> list[list[str]]:
     table = [[letter for letter in line] for line in data.splitlines()]
-    return np.matrix(table)
+    return table
 
 
-def number_length(num_position: int, table: np.matrix) -> int:
+def number_length(num_position: int, table: list) -> int:
     length = 0
     while True:
         try:
@@ -58,11 +48,34 @@ def number_length(num_position: int, table: np.matrix) -> int:
             return length
 
 
-def extract_surroundings(num_position: int, table: np.matrix) -> str:
+def extract_surroundings(num_position: int, table: list) -> str:
     return ""
+
+
+# END LEGACY
+
+
+def parse_data_as_dict(data: str) -> dict:
+    table = {}
+    for y, line in enumerate(data.splitlines()):
+        for x, letter in enumerate(line):
+            table[(x, y)] = letter
+    return table
+
+
+def get_all_num_positions(table: dict) -> list[str]:
+    for i in table:
+        if table[i].isnumeric():
+            yield i
+
+
+def is_part_number(position: int) -> bool:
+    return False
 
 
 if __name__ == "__main__":
     with open("input.txt", "r") as f:
         data = f.read()
-    print(parse_data(data))
+    plop = parse_data_as_dict(data)
+    for i in get_all_num_positions(plop):
+        print(i, plop[i])
