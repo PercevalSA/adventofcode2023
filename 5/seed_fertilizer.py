27a18cd693fbe7e238a485de7bee9adba0657b0d
Perceval ARENOU <perceval.arenou@gmail.com>
2023-12-18 01:24:18 +0100

    day 5: first naïve parser

--- 5/seed_fertilizer.py
+++ 5/seed_fertilizer.py
@@ -1,20 +1,68 @@
 #!/usr/bin/python3
 
-
-def parse_data(data: str) -> list:
-    return data.splitlines()
+from collections import defaultdict 
 
 
-def solve_part_1(data: list) -> int:
-    return 0
+# we can use defaultdict to generat missing part as it is only int to int correspondances
+# default dict does not take any argument to generate missing parts we need our own dict type
+class MyDict(dict):
+    __missing__ = lambda self, key: key
 
+class FieldAnalyzer:
+    def __init__(self, data_src: str):
+        self._data_src = data_src
+        self._data = self._parse_data(self._data_src)
+    
+    @property
+    def data(self):
+        return self._data
+
+    @data.setter
+    def data(self, value):
+        self._data = value
+
+    @data.deleter
+    def data(self):
+        del self._data
+
+
+    def _parse_numbers(self, data: str) -> dict[int, int]:
+        correspondance = MyDict()
+        for line in data.splitlines():
+            dest, src, ran = line.split()
+            for i in range(int(ran)):
+                correspondance[int(src) + i] = int(dest) + i
+
+        return correspondance
+
+    def _parse_block(self, data: str) -> tuple:
+        data_name, data_content = data.split(":")
+        data_content = self._parse_numbers(data_content)
+
+        return data_name, data_content
+
+    def _parse_data(self, data: str) -> dict:
+        """each block is seperated by a line gap, then a title followed by:
+        finally all numbers are parsed the same way"""
+        parsed_data = {}
+        # split by block before parsing each data
+        for block in data.split("\n\n"):
+            data_name, data_content = self._parse_block(block)
+            parsed_data[data_name] = [data_content]
+
+        return parsed_data
+
+
+    def solve_part_1(self) -> int:
+        return 0
 
 def main(file: str):
     with open(file, "r") as f:
         data = f.read()
-    parsed_data = parse_data(data)
 
-    result = solve_part_1(parsed_data)
+    filed_analyzer = FieldAnalyzer(data)
+
+    result = filed_analyzer.solve_part_1()
     print(f"Result 1: {result}")
 
 
