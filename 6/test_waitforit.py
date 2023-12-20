31e3ac9652939d51c4d175c38fbb3b9847bdce31
Perceval ARENOU <perceval.arenou@gmail.com>
2023-12-20 01:09:02 +0100

    day 6: part 2

--- 6/test_waitforit.py
+++ 6/test_waitforit.py
@@ -21,3 +21,20 @@ def test_maximum():
     assert waitforit.find_maximum_button_time(data[0]) == 5
     assert waitforit.find_maximum_button_time(data[1]) == 11
     assert waitforit.find_maximum_button_time(data[2]) == 19
+
+
+def test_count_ways_to_solve():
+    data = waitforit.parse_input(input)
+    assert waitforit.count_ways_to_solve(data[0]) == 4
+    assert waitforit.count_ways_to_solve(data[1]) == 8
+    assert waitforit.count_ways_to_solve(data[2]) == 9
+
+def test_part_1():
+    data = waitforit.parse_input(input)
+    assert waitforit.solve_part_1(data) == 288
+
+def test_parsed_data():
+    data = waitforit.parse_part_2(input)
+    assert data == (71530, 940200)
+
+# 71503 ways
--- 6/waitforit.py
+++ 6/waitforit.py
@@ -4,6 +4,9 @@ def parse_input(data: str) -> list[tuple[int, int]]:
     times, distances = [line.split()[1:] for line in data.splitlines()]
     return [(int(t), int(d)) for t, d in zip(times, distances)]
 
+def parse_part_2(data: str) -> tuple[int,int]:
+    time, distance = ["".join(line.split()[1:]) for line in data.splitlines()]
+    return (int(time), int(distance))
 
 def distance_from_button_pressed_time(button_time: int, total_time: int) -> int:
     sailing_time = total_time - button_time
@@ -11,17 +14,30 @@ def distance_from_button_pressed_time(button_time: int, total_time: int) -> int:
     return sailed_distance
 
 
-def find_minimum_button_time(race: tuple):
+def find_minimum_button_time(race: tuple) -> int:
     total_time, distance_to_beat = race
     for i in range(total_time):
         if distance_from_button_pressed_time(i, total_time) > distance_to_beat:
             return i
 
-def find_maximum_button_time(race: tuple):
+
+def find_maximum_button_time(race: tuple) -> int:
     total_time, distance_to_beat = race
     for i in reversed(range(total_time)):
         if distance_from_button_pressed_time(i, total_time) > distance_to_beat:
             return i
+
+
+def count_ways_to_solve(race: tuple) -> int:
+    return find_maximum_button_time(race) - find_minimum_button_time(race) + 1
+
+
+def solve_part_1(data: list):
+    result = 1
+    for race in data:
+        result = result * count_ways_to_solve(race)
+    return result
+
 
 # one full millisecond hold = one millimeter per millisecond
 def main(file: str):
@@ -29,13 +45,10 @@ def main(file: str):
         data = f.read()
 
     parsed_data = parse_input(data)
-    for i in parsed_data:
-        min = find_minimum_button_time(i)
-        max = find_maximum_button_time(i)
-        print(min, max)
-    
-    result = None
+    result = solve_part_1(parsed_data)
     print(f"Result 1: {result}")
+
+    parsed_data = parse_part_2(data)
 
 
 if __name__ == "__main__":
