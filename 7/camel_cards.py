5c520c3ba7e6933d116f92b09a9955a91f73a37a
PercevalSA <perceval.arenou@gmail.com>
2023-12-22 23:14:47 +0000

    day 7: start

--- 7/camel_cards.py
+++ 7/camel_cards.py
@@ -1,17 +1,72 @@
 #!/usr/bin/python3
 
-strength_order = ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"]
+strength_order = {
+    "A": 13,
+    "K": 12,
+    "Q": 11,
+    "J": 10,
+    "T": 9,
+    "9": 8,
+    "8": 7,
+    "7": 6,
+    "6": 5,
+    "5": 4,
+    "4": 3,
+    "3": 2,
+    "2": 1,
+}
 
-"""
-victory
-Five of a kind, where all five cards have the same label: AAAAA
-Four of a kind, where four cards have the same label and one card has a different label: AA8AA
-Full house, where three cards have the same label, and the remaining two cards share a different label: 23332
-Three of a kind, where three cards have the same label, and the remaining two cards are each different from any other card in the hand: TTT98
-Two pair, where two cards share one label, two other cards share a second label, and the remaining card has a third label: 23432
-One pair, where two cards share one label, and the other three cards have a different label from the pair and each other: A23A4
-High card, where all cards' labels are distinct: 23456
-"""
+
+def is_5_of_kind(hand: str) -> bool:
+    """Five of a kind, where all five cards have the same label: AAAAA"""
+    cards = list(hand)
+    for i in range(len(cards) - 1):
+        if cards[i] != cards[i + 1]:
+            return False
+    return True
+
+
+def is_4_of_kind(hand: str) -> bool:
+    """Four of a kind, where four cards have the same label
+    and one card has a different label: AA8AA
+    """
+    return False
+
+
+def is_full_house(hand: str) -> bool:
+    """Full house, where three cards have the same label,
+    and the remaining two cards share a different label: 23332
+    """
+    return False
+
+
+def is_3_of_kind(hand: str) -> bool:
+    """Three of a kind, where three cards have the same label,
+    and the remaining two cards are each different
+    from any other card in the hand: TTT98
+    """
+    return False
+
+
+def is_2_pairs(hand: str) -> bool:
+    """Two pair, where two cards share one label,
+    two other cards share a second label,
+    and the remaining card has a third label: 23432
+    """
+    return False
+
+
+def is_1_pair(hand: str) -> bool:
+    """One pair, where two cards share one label,
+    and the other three cards have a different label
+    from the pair and each other: A23A4
+    """
+    return False
+
+
+def is_high_card(hand: str) -> bool:
+    """High card, where all cards' labels are distinct: 23456"""
+    return False
 
 
 def parse_input(data: str) -> list:
--- 7/test_camel_cards.py
+++ 7/test_camel_cards.py
@@ -1,5 +1,13 @@
+import camel_cards
+
 data = """32T3K 765
 T55J5 684
 KK677 28
 KTJJT 220
 QQQJA 483"""
+
+
+def test_is_5_of_kind():
+    assert camel_cards.is_5_of_kind("AAAAA") is True
+    assert camel_cards.is_5_of_kind("33344") is False
+    assert camel_cards.is_5_of_kind("5TTTT") is False
